716722a19751fe5b03d2ee24f18c26f8dc277943
himuraxkenji <aaron.ariperto@gmail.com>
2023-04-13 12:49:54 -0300

    add sass, assets and change structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './routes/App'
+import MyApp from 'pages/_app'
 
-ReactDOM.render(<App/>, document.getElementById("app"))
+ReactDOM.render(<MyApp/>, document.getElementById("app"))
