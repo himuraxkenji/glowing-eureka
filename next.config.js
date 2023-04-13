a90ee5633b1cdcda9f3c5186557139db12a2a22b
himuraxkenji <aaron.ariperto@gmail.com>
2023-04-13 18:13:07 -0300

    add seo and google analitics

--- next.config.js
+++ next.config.js
@@ -1,22 +1,28 @@
 /** @type {import('next').NextConfig} */
+const hostnames = [
+  'placeimg.com',  
+  'api.lorem.space',
+  'placeimh.com',
+  'picsum.photos',
+  'upcdn.io',
+  'www.elgrafico.com.ar',
+  'api.escuelajs.co',
+  'educacion30.b-cdn.net',
+  'img.freepik.com',
+  'media.licdn.com',
+  'picsum.photos',
+  'placeimg.com',
+  'images.pexels.com'
+]
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos'
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.licdn.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'placeimg.com'
-      }
-    ]
+    remotePatterns: hostnames.map(host => ({
+      protocol: 'https',
+      hostname: host
+    }))
   }
 }
 
